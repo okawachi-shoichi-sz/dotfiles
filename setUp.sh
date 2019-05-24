cd4fef304a35d97c451e6995bc1bac89b3c2b86e
shoichi1023 <so_1117@outlook.jp>
2019-05-24 12:48:23 +0900

    add setup script

--- setUp.sh
+++ setUp.sh
@@ -1,18 +1,17 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew update
-brew upgrade
+apt update
+apt upgrade
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cp .tmux.conf_mac ~/.tmux.conf
-brew install ctags peco translate-shell reattach-to-user-namespace
+cp .tmux.conf ~/.tmux.conf
+sudo apt install -y peco translate-shell xsel
 cp ./dein/.vimrc ~/.vimrc
 mkdir ~/.vim/rc/
 cp ./dein/plugin.toml ~/.vim/rc/plugin.toml
 cp ./.bashrc_add ~/.bashrc
-cp ./.bash_profile ~/.eash_profile
-brew install nodebrew
+cp ./.bash_profile ~/.bash_profile
+sudo apt install -y nodebrew
 nodebrew setup
 nodebrew install-binary latest
 nodebrew use latest
 npm install -g prettier-eslint
-brew install golang
+sudo apt install -y golang
 mkdir ~/src/github.com/
